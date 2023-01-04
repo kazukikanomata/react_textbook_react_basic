0b9be9c5f0b7bbc0c42a93ac8e8fd1ee32bb0b28
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2023-01-04 19:48:40 +0900

    add：アッパーキャメルで書いてみた

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 export const App = () => {
+    const onClickButton = () =>{
+        alert("すげー表示された");
+    };
     return(
         // 1つのタグで囲んでいこう
         // fragment便利すぎ
         <>
+            { console.log("TEST") }
             <h1>Hello World!!</h1>
             <p>お元気ですか？</p>
+            <button onClick={ onClickButton }>ボタン</button>
         </>
     );
 };
