5a9105c82d1f98f663f0bb4914017dfe819f7ac8
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2023-01-04 19:28:22 +0900

    add:jsx記法を書いてみた

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export const App = () => {
+    return(
+        // 1つのタグで囲んでいこう
+        // fragment便利すぎ
+        <>
+            <h1>Hello World!!</h1>
+            <p>お元気ですか？</p>
+        </>
+    );
+};
