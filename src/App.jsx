52ab41af17a874fa4c6adf93979f821c224995e2
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2023-01-06 20:22:04 +0900

    add：styleを追加する

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ export const App = () => {
     const onClickButton = () =>{
         alert("すげー表示された");
     };
+    const contentStyle ={
+        color: "blue",
+        fontSize: "20px"
+    };
     return(
         // 1つのタグで囲んでいこう
         // fragment便利すぎ
         <>
             { console.log("TEST") }
-            <h1>Hello World!!</h1>
-            <p>お元気ですか？</p>
+            <h1 style={{ color : "red" }}>Hello World!!</h1>
+            <p style={ contentStyle }>お元気ですか？</p>
             <button onClick={ onClickButton }>ボタン</button>
         </>
     );
