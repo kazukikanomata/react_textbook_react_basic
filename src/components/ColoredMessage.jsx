90a037a352e1ac52b478ae347ae6cca5c72a033c
kazukikanomata <kazu.surface.pro2020@outlook.jp>
2023-01-06 20:55:28 +0900

    add：destructureしてみた

--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,7 +1,12 @@
 export const ColoredMessage = (props) => {
+    // propsを分割代入
+    const { color, fontSize, children } = props;
+
+    // 省略記法
     const contentStyle ={
-        color: props.color,
-        fontSize: props.fontSize
+        color,
+        fontSize
     };
-    return <p style={ contentStyle }>{ props.children }</p>;
+
+    return <p style={ contentStyle }>{ children }</p>;
 };
